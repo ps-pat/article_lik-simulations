9dada278e43904e3434ade34787e15d70c4b928b
Patrick Fournier <p_fournier@hushmail.com>
2023-10-04 13:42:38 -0400

    Make `sample_prop` an argument tu `study1`.

--- src/pure_coal.jl
+++ src/pure_coal.jl
@@ -254,9 +254,8 @@ export study1
 
 Execute the first simulation study.
 """
-function study1(path = "study1.data"; kwargs...)
+function study1(path = "study1.data"; sample_prop = 1e-4, kwargs...)
     rng = Xoshiro(42)
-    sample_prop = 1e-4
     scenarios = Dict(:full => (wild = 0.05, derived = 1.0),
                      :high => (wild = 0.05, derived = 0.75),
                      :low => (wild = 0.05, derived = 0.2))
